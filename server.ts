e4685320a9740093d3919fdd0cd94fe605d0918f
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-01-06 14:33:10 -0600

    Added test route in server.ts

--- server.ts
+++ server.ts
@@ -1,18 +1,22 @@
-const express = require("express");
+import express,{ Request, Response } from 'express';
 const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
+
 connectDb();
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
+app.get('/', (req: Request, res : Response) => {
+  res.send('New Orleans Food Spots Server');
+});
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
